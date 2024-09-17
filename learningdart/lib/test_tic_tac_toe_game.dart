3ba5c2a2b65edd947e16e5f05f12557f9a332f35
David Fisher <fisherds@rose-hulman.edu>
2024-09-17 12:59:12 -0400

    TicTacToe Unit tests

--- learningdart/lib/test_tic_tac_toe_game.dart
+++ learningdart/lib/test_tic_tac_toe_game.dart
@@ -3,32 +3,190 @@ import 'package:learningdart/tic_tac_toe_game.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // group('String', () {
-  //   test('.split() splits the string on the delimiter', () {
-  //     var string = 'foo,bar,baz';
-  //     expect(string.split(','), equals(['foo', 'bar', 'baz']));
-  //   });
-
-  //   test('.trim() removes surrounding whitespace', () {
-  //     var string = '  foo ';
-  //     expect(string.trim(), equals('foo'));
-  //   });
-  // });
-
   late TicTacToeGame game;
   setUp(() {
     game = TicTacToeGame();
   });
+  // tearDown(() {
+  //   print("Teardown"); // Remove this later.
+  // });
 
-  tearDown(() {
-    print("Teardown"); // Remove this later.
+  group('Test the constructor:', () {
+    test('Check the game state', () {
+      expect(game.state, equals(TicTacToeState.xTurn));
+    });
+    test('Check the initial board', () {
+      for (int k = 0; k < 9; k++) {
+        expect(game.board[k], equals(TicTacToeMark.none));
+      }
+    });
   });
 
-  group('Test the constructor', () {
-    test('Check the game state', () {});
+  group("Non-win moves:", () {
+    test('Check 1 move', () {
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      for (int k = 0; k < 9; k++) {
+        if (k == 4) {
+          expect(game.board[k], equals(TicTacToeMark.x));
+        } else {
+          expect(game.board[k], equals(TicTacToeMark.none));
+        }
+      }
+    });
+    test("2 Moves", () {
+      game.pressedSquare(4);
+      game.pressedSquare(0);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      for (int k = 0; k < 9; k++) {
+        if (k == 0) {
+          expect(game.board[k], equals(TicTacToeMark.o));
+        } else if (k == 4) {
+          expect(game.board[k], equals(TicTacToeMark.x));
+        } else {
+          expect(game.board[k], equals(TicTacToeMark.none));
+        }
+      }
+    });
+    test("Out of range index: ", () {
+      game.pressedSquare(-2);
+      game.pressedSquare(9);
+      // Same as the initial state
+      expect(game.state, equals(TicTacToeState.xTurn));
+      for (int k = 0; k < 9; k++) {
+        expect(game.board[k], equals(TicTacToeMark.none));
+      }
+    });
+    test("moves to squares that are not none", () {
+      game.pressedSquare(4);
+      game.pressedSquare(4);
+      game.pressedSquare(4);
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      for (int k = 0; k < 9; k++) {
+        if (k == 4) {
+          expect(game.board[k], equals(TicTacToeMark.x));
+        } else {
+          expect(game.board[k], equals(TicTacToeMark.none));
+        }
+      }
+    });
+  });
 
-    test('Check the initial board', () {});
-
-    test('Check the board/state after a few changes', () {});
+  group("Wins (and ties)", () {
+    test("X Vertical win:", () {
+      // Planned Xs: 2 5 8
+      // Planned Os: 3 7
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(2);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(5);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(7);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.xWon));
+    });
+    test("O Horizontal win:", () {
+      // Planned Xs: 0 1 8
+      // Planned Os: 3 4 5
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(0);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(1);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(5);
+      expect(game.state, equals(TicTacToeState.oWon));
+    });
+    test("O Diagonal win:", () {
+      // Planned Xs: 2 4 6
+      // Planned Os: 3 8
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(2);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(6);
+      expect(game.state, equals(TicTacToeState.xWon));
+    });
+    test("Tie game", () {
+      // Planned Xs: 0 1 5 6 8
+      // Planned Os: 3 4 2 7
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(0);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(1);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(5);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(2);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(6);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(7);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.tie));
+    });
+  });
+  group("Edge cases:", () {
+    test("Playing beyond a win", () {
+      // Planned Xs: 2 5 8
+      // Planned Os: 3 7
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(2);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(5);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(7);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.xWon));
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.xWon));
+      game.pressedSquare(1);
+      expect(game.state, equals(TicTacToeState.xWon));
+    });
+    test("X win on move 9", () {
+      // Planned Xs: 0 1 5 6 8
+      // Planned Os: 3 4 2 7
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(0);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(3);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(1);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(4);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(5);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(2);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(6);
+      expect(game.state, equals(TicTacToeState.oTurn));
+      game.pressedSquare(7);
+      expect(game.state, equals(TicTacToeState.xTurn));
+      game.pressedSquare(8);
+      expect(game.state, equals(TicTacToeState.tie));
+    });
   });
 }
--- learningdart/lib/tic_tac_toe_game.dart
+++ learningdart/lib/tic_tac_toe_game.dart
@@ -46,16 +46,62 @@ class TicTacToeGame {
   String toString() {
     return "$stateString $boardString";
   }
+
+  void pressedSquare(int index) {
+    if (index < 0 || index > 8) {
+      return; // invalid input
+    }
+    if (board[index] != TicTacToeMark.none) {
+      return; // square is not available.
+    }
+    if (state == TicTacToeState.xTurn) {
+      board[index] = TicTacToeMark.x;
+      state = TicTacToeState.oTurn;
+    } else if (state == TicTacToeState.oTurn) {
+      board[index] = TicTacToeMark.o;
+      state = TicTacToeState.xTurn;
+    } else {
+      return; // Not allowed to take more turns, this game is over.
+    }
+    checkForWin();
+  }
+
+  void checkForWin() {
+    // Check for ties first.
+    if (!board.contains(TicTacToeMark.none)) {
+      state = TicTacToeState.tie;
+    }
+    // Check for wins second (can override ties)
+    var linesOf3 = [];
+    linesOf3.add(_stringFromIndices(0, 1, 2));
+    linesOf3.add(_stringFromIndices(3, 4, 5));
+    linesOf3.add(_stringFromIndices(6, 7, 8));
+    linesOf3.add(_stringFromIndices(0, 3, 6));
+    linesOf3.add(_stringFromIndices(1, 4, 7));
+    linesOf3.add(_stringFromIndices(2, 5, 8));
+    linesOf3.add(_stringFromIndices(0, 4, 8));
+    linesOf3.add(_stringFromIndices(2, 4, 6));
+    for (var lineOf3 in linesOf3) {
+      if (lineOf3 == "XXX") {
+        state = TicTacToeState.xWon;
+      } else if (lineOf3 == "OOO") {
+        state = TicTacToeState.oWon;
+      }
+    }
+  }
+
+  String _stringFromIndices(int a, int b, int c) =>
+      boardString[a] + boardString[b] + boardString[c];
 }
 
-void main() {
-  print("Quick and dirty testing (pre-unit testing)");
-  TicTacToeMark mark = TicTacToeMark.x;
-  print(mark);
-  var state = TicTacToeState.xWon;
-  print(state);
-  var game = TicTacToeGame();
-  game.board[4] = TicTacToeMark.x;
-  game.board[8] = TicTacToeMark.o;
-  print(game);
-}
+// void main() {
+//   print("Quick and dirty testing (pre-unit testing)");
+//   TicTacToeMark mark = TicTacToeMark.x;
+//   print(mark);
+//   var state = TicTacToeState.xWon;
+//   print(state);
+//   var game = TicTacToeGame();
+//   game.board[4] = TicTacToeMark.x;
+//   game.board[8] = TicTacToeMark.o;
+//   print(game);
+// }
